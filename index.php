833c05e5e52fff491cd0906e4a3b3f6c481da3d7
Gabriela Diniz <gadisa10@outlook.com>
2022-12-30 00:10:35 -0300

    estilizando page

--- index.php
+++ index.php
@@ -6,28 +6,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/estilo.css" rel="stylesheet">
+    <!-- link ícones bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <title>Curso Get started with PHP</title>
+    <title>Get started with PHP</title>
 </head>
 
 <body>
+    <div id="texto">
+    <p><span class="bi bi-arrow-right"></span> Exercício da trilha de aprendizado em PHP!</p>
+    </div>
     <nav>
+    <a href="https://github.com/gabrieladnz" target="_blank"><button type="button" id="botGit">gabrieladnz <span class="bi bi-github"></span></button></a>
+    <a href="https://www.linkedin.com/in/gabrieladnz/" target="_blank"><button type="button" id="botLinkd"><span class="bi bi-linkedin"></span></button></a>
+    <a href="mailto:gabrieladnz.dev@gmail.com?" target="_blank"><button type="button" id="botEmail"><span class="bi bi-envelope-fill"></span></button></a>
+</nav>
 
-    </nav>
     <!-- formulário de envio de email -->
-    <form>
-        <label>Destinatário:</label><input>
-        <label>Mensagem:</label><input>
-    </form>
-
-    <?php
+    <section id="formEmail">
+        <br>
+        <h4 id="tituloEmail">EMAIL FÁCIL</h4>
+        <form>
+            <label id="labelEmail">Nome:</label><input><br>
+            <label id="labelEmail">Destinatário:</label><input><br>
+            <label id="labelEmail">Mensagem:</label><input>
+        </form>
+    </section>
+    <!--
     // função que lê arquivo e exibe na página
     readfile('doc/teste.txt');
-    ?>
+    -->
 </body>
-<footer>
-
-</footer>
-
 </html>
